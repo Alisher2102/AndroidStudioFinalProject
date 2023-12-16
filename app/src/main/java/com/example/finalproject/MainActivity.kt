ab309f2e65d1f8d7149177319b5e3c265d2a32ee
welray17 <temirlan2003@list.ru>
2023-12-16 20:07:12 +0600

    creating my_playlist page

--- app/src/main/java/com/example/finalproject/MainActivity.kt
+++ app/src/main/java/com/example/finalproject/MainActivity.kt
@@ -1,6 +1,7 @@
 package com.example.finalproject
 
 import android.content.ContentValues.TAG
+import android.media.MediaPlayer
 import android.os.Bundle
 import android.util.Log
 import android.widget.TextView
@@ -13,6 +14,9 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.example.finalproject.model.DataAdapter
 import com.example.finalproject.model.MusicData
 import com.example.finalproject.network.MusicApi
 import com.example.finalproject.ui.theme.materials.FinalProjectTheme
@@ -21,18 +25,30 @@ import retrofit2.Callback
 import retrofit2.Response
 
 class MainActivity : ComponentActivity() {
+    lateinit var recyclerView: RecyclerView
+    lateinit var adapter: DataAdapter
+    companion object {
+        val globalMediaPlayer = MediaPlayer()
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        recyclerView = findViewById(R.id.recyclerView)
         val retrofitData = MusicApi.retrofitService.getData("eminem")
         retrofitData.enqueue(object : Callback<MusicData?> {
             override fun onResponse(
                 call: Call<MusicData?>,
                 response: Response<MusicData?>
             ) {
-                val dataList = response.body()?.data
-                val textView = findViewById<TextView>(R.id.textView)
-                textView.text = dataList.toString()
+                val dataList = response.body()?.data!!
+//                val textView = findViewById<TextView>(R.id.textView)
+//                textView.text = dataList.toString()
+                adapter = DataAdapter(this@MainActivity, dataList)
+                recyclerView.adapter = adapter
+                recyclerView.layoutManager = LinearLayoutManager(this@MainActivity)
+
                 Log.d("TAG: onResponse", "onResponse: " + response.body())
             }
 
--- app/src/main/java/com/example/finalproject/model/MusicData.kt
+++ app/src/main/java/com/example/finalproject/model/MusicData.kt
@@ -1,7 +1,7 @@
 package com.example.finalproject.model
 
 data class MusicData(
-    val `data`: List<Data> = emptyList(),
+    val data: List<Data> = emptyList(),
     val next: String = "",
     val total: Int = 0
 )
