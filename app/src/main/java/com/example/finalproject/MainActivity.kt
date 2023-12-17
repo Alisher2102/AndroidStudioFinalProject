368ebb9ebc3a474e8d4a9cfa169e1fb4442b801e
welray17 <119338912+welray17@users.noreply.github.com>
2023-12-17 20:20:57 +0600

    Update MainActivity.kt

--- app/src/main/java/com/example/finalproject/MainActivity.kt
+++ app/src/main/java/com/example/finalproject/MainActivity.kt
@@ -3,11 +3,10 @@ package com.example.finalproject
 import android.media.MediaPlayer
 import android.os.Bundle
 import android.util.Log
-import android.widget.SearchView
-import android.widget.SearchView.OnQueryTextListener
 import androidx.activity.ComponentActivity
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
+import com.example.finalproject.data.MyPlaylistDataProvider
 import com.example.finalproject.model.MusicData
 import com.example.finalproject.network.MusicApi
 import retrofit2.Call
@@ -16,48 +15,34 @@ import retrofit2.Response
 
 class MainActivity : ComponentActivity() {
     lateinit var recyclerView: RecyclerView
-    lateinit var adapter: DataAdapter
-    lateinit var searchView: SearchView
+    lateinit var adapter: MyPlaylistAdapter
     companion object {
         val globalMediaPlayer = MediaPlayer()
     }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-        searchView = findViewById<SearchView>(R.id.sv)
+        setContentView(R.layout.list_of_music)
+
         recyclerView = findViewById(R.id.recyclerView)
-        searchView.setOnQueryTextListener(object : OnQueryTextListener{
-            override fun onQueryTextSubmit(query: String?): Boolean {
-                return true
+        val retrofitData = MusicApi.retrofitService.getData("eminem")
+        retrofitData.enqueue(object : Callback<MusicData?> {
+            override fun onResponse(
+                call: Call<MusicData?>,
+                response: Response<MusicData?>
+            ) {
+//                val dataList = response.body()?.data!!
+//                adapter = DataAdapter(this@MainActivity, dataList)
+                val dataList = MyPlaylistDataProvider.getTracksData()
+                adapter = MyPlaylistAdapter(this@MainActivity, dataList)
+                recyclerView.adapter = adapter
+                recyclerView.layoutManager = LinearLayoutManager(this@MainActivity)
+
+                Log.d("TAG: onResponse", "onResponse: " + response.body())
             }
 
-            override fun onQueryTextChange(newText: String?): Boolean {
-                val retrofitData = newText?.let { MusicApi.retrofitService.getData(it) }
-                retrofitData?.enqueue(object : Callback<MusicData?> {
-                    override fun onResponse(
-                        call: Call<MusicData?>,
-                        response: Response<MusicData?>
-                    ) {
-                        val dataList = response.body()?.data!!
-
-                        //                val textView = findViewById<TextView>(R.id.textView)
-                        //                textView.text = dataList.toString()
-
-            //                val textView = findViewById<TextView>(R.id.textView)
-            //                textView.text = dataList.toString()
-                        adapter = DataAdapter(this@MainActivity, dataList)
-                        recyclerView.adapter = adapter
-                        recyclerView.layoutManager = LinearLayoutManager(this@MainActivity)
-
-                        Log.d("TAG: onResponse", "onResponse: " + response.body())
-                    }
-
-                    override fun onFailure(call: Call<MusicData?>, t: Throwable) {
-                        Log.d("TAG: onFailure", "onFailure: " + t.message)
-                    }
-                })
-                return true
+            override fun onFailure(call: Call<MusicData?>, t: Throwable) {
+                Log.d("TAG: onFailure", "onFailure: " + t.message)
             }
         })
     }
