d5707a6e4a5d11a1dfc44667201435592a76ca0b
Alisher <recker2014brr@gmail.com>
2023-12-18 03:54:35 +0600

    search in downloaded songs

--- app/src/main/java/com/example/finalproject/MainActivity.kt
+++ app/src/main/java/com/example/finalproject/MainActivity.kt
@@ -4,53 +4,69 @@ import android.content.Intent
 import android.media.MediaPlayer
 import android.os.Bundle
 import android.util.Log
-import android.view.View
-import android.widget.SeekBar
+import android.widget.ImageView
+import android.widget.SearchView
+import android.widget.SearchView.OnQueryTextListener
 import androidx.activity.ComponentActivity
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.example.finalproject.data.MyPlaylistDataProvider
-import com.example.finalproject.model.MusicData
-import com.example.finalproject.network.MusicApi
-import retrofit2.Call
-import retrofit2.Callback
-import retrofit2.Response
+import com.example.finalproject.model.Track
 
 class MainActivity : ComponentActivity() {
     lateinit var recyclerView: RecyclerView
     lateinit var adapter: MyPlaylistAdapter
+    lateinit var searchView: SearchView
     companion object {
         val globalMediaPlayer = MediaPlayer()
     }
-    lateinit var seekbar: SeekBar
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.list_of_music)
-
+        searchView = findViewById(R.id.search)
         recyclerView = findViewById(R.id.recyclerView)
-        val retrofitData = MusicApi.retrofitService.getData("eminem")
-        retrofitData.enqueue(object : Callback<MusicData?> {
-            override fun onResponse(
-                call: Call<MusicData?>,
-                response: Response<MusicData?>
-            ) {
-//                val dataList = response.body()?.data!!
-//                adapter = DataAdapter(this@MainActivity, dataList)
-                val dataList = MyPlaylistDataProvider.getTracksData()
-                adapter = MyPlaylistAdapter(this@MainActivity, dataList)
-                recyclerView.adapter = adapter
-                seekbar = findViewById(R.id.musicSeekBar)
-                recyclerView.layoutManager = LinearLayoutManager(this@MainActivity)
 
-                Log.d("TAG: onResponse", "onResponse: " + response.body())
+        val dataList = MyPlaylistDataProvider.getTracksData()
+        adapter = MyPlaylistAdapter(this@MainActivity, dataList)
+        recyclerView.adapter = adapter
+        recyclerView.layoutManager = LinearLayoutManager(this@MainActivity)
+        Log.d("TAG: DataList", "Check : $dataList")
+        searchView.setOnQueryTextListener(object : OnQueryTextListener{
+            override fun onQueryTextSubmit(query: String?): Boolean {
+                return true
             }
 
-            override fun onFailure(call: Call<MusicData?>, t: Throwable) {
-                Log.d("TAG: onFailure", "onFailure: " + t.message)
+            override fun onQueryTextChange(newText: String?): Boolean {
+                newText?.let { query ->
+                    val filteredTracks = getFilteredTracks(query)
+                    val adapter = MyPlaylistAdapter(this@MainActivity, filteredTracks)
+                    recyclerView.adapter = adapter
+                    recyclerView.layoutManager = LinearLayoutManager(this@MainActivity)
+                }
+                return true
             }
         })
+        searchButton()
+        homeButton()
     }
-    fun startActivity(view: View){
-        intent = Intent(this, MainActivity2::class.java)
+    private fun getFilteredTracks(query: String): List<Track> {
+        return MyPlaylistDataProvider.getTracksData().filter {
+            it.title.contains(query, ignoreCase = true)
+        }
+    }
+    private fun searchButton(){
+        val searchSp = findViewById<ImageView>(R.id.svButton)
+        searchSp.setOnClickListener(){
+            val intent = Intent(this,MainActivity2::class.java)
+            startActivity(intent)
+        }
+    }
+    private fun homeButton(){
+        val homeSp = findViewById<ImageView>(R.id.HOME)
+        homeSp.setOnClickListener(){
+            val intent = Intent(this,MainActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
