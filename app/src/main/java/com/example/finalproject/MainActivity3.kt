0861ecfda34a3eeca6080e5c01e27d1b4f3129e5
Alisher <recker2014brr@gmail.com>
2023-12-18 23:40:13 +0600

    xdxddx

--- app/src/main/java/com/example/finalproject/MainActivity3.kt
+++ app/src/main/java/com/example/finalproject/MainActivity3.kt
@@ -3,28 +3,15 @@ package com.example.finalproject
 import android.content.Intent
 import android.net.Uri
 import android.os.Bundle
-import android.os.Handler
 import android.view.View
-import android.widget.ImageButton
 import android.widget.ImageView
 import android.widget.SeekBar
 import android.widget.TextView
 import androidx.activity.ComponentActivity
-import androidx.activity.compose.setContent
 import androidx.activity.viewModels
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
-import androidx.recyclerview.widget.LinearLayoutManager
-import com.example.finalproject.MainActivity.Companion.globalMediaPlayer
 import com.example.finalproject.data.MyPlaylistDataProvider
 import com.example.finalproject.model.MusicPlayerVIewModel
 import com.example.finalproject.model.Track
-import com.example.finalproject.ui.theme.FinalProjectTheme
 
 class MainActivity3 : ComponentActivity() {
     var musicId: Int = -1
@@ -35,29 +22,20 @@ class MainActivity3 : ComponentActivity() {
     lateinit var musicImage: ImageView
     lateinit var trackName: TextView
     lateinit var artist: TextView
-    lateinit var playBtn: ImageButton
     val musicPlayerViewModel: MusicPlayerVIewModel by viewModels()
-    val handler = Handler()
-
     override fun onCreate(savedInstanceState: Bundle?) {
 
         super.onCreate(savedInstanceState)
         setContentView(R.layout.one_track)
-        musicId = intent.getIntExtra("music_id", -1)
-        playingTrack = intent.getIntExtra("playing_track", -1)
         seekbar = findViewById(R.id.musicSeekBar)
-        if(musicId == playingTrack)
-            startSeekBar()
         musicImage = findViewById(R.id.music_image)
         trackName = findViewById(R.id.title)
         artist = findViewById(R.id.artist)
-        playBtn = findViewById(R.id.play)
+        musicId = intent.getIntExtra("music_id", -1)
         tracksList = MyPlaylistDataProvider.getTracksData()
         selectedTrack = tracksList.find { it.id == musicId }!!
+//        playingTrack = intent.getIntExtra("playing_track", -1)
 
-        if (musicId == playingTrack && globalMediaPlayer.isPlaying)
-            playBtn.setImageResource(R.drawable.pause)
-        else playBtn.setImageResource(R.drawable.play_button)
         musicImage.setImageResource(selectedTrack.imageId)
         trackName.text = selectedTrack.title
         artist.text = selectedTrack.artist
@@ -69,54 +47,28 @@ class MainActivity3 : ComponentActivity() {
     }
 
     fun play(view: View){
-        startSeekBar()
+        val audioPath = intent.getStringExtra("audio_path")
+        val isPlaying = intent.getBooleanExtra("is_playing", false)
+        val currentPosition = intent.getIntExtra("current_position", 0)
+        playingTrack = intent.getIntExtra("playing_track", -1)
         if (musicId != playingTrack) {
             if (playingTrack != -1) {
-                globalMediaPlayer.stop()
-                globalMediaPlayer.reset()
+                MainActivity.globalMediaPlayer.stop()
+                MainActivity.globalMediaPlayer.reset()
             }
-            globalMediaPlayer.setDataSource(
+            MainActivity.globalMediaPlayer.setDataSource(
                 this,
-                Uri.parse("android.resource://${this.packageName}/${selectedTrack.audio}")
+                Uri.parse("android.resource://${this.packageName}/${selectedTrack?.audio}")
             )
-            globalMediaPlayer.prepare()
-            globalMediaPlayer.start()
-            playBtn.setImageResource(R.drawable.pause)
+            MainActivity.globalMediaPlayer.prepare()
+            MainActivity.globalMediaPlayer.start()
             playingTrack = musicId
         }else{
-            if (globalMediaPlayer.isPlaying) {
-                globalMediaPlayer.pause()
-                playBtn.setImageResource(R.drawable.play_button)
+            if (MainActivity.globalMediaPlayer.isPlaying) {
+                MainActivity.globalMediaPlayer.pause()
             } else {
-                globalMediaPlayer.start()
-                playBtn.setImageResource(R.drawable.pause)
+                MainActivity.globalMediaPlayer.start()
             }
         }
-    }
-    fun startSeekBar(){
-        seekbar.max = globalMediaPlayer.duration
-        val currentPosition = intent.getIntExtra("current_position",0)
-        seekbar.progress = currentPosition
-        seekbar.setOnSeekBarChangeListener(object : SeekBar.OnSeekBarChangeListener {
-            override fun onProgressChanged(seekBar: SeekBar?, progress: Int, fromUser: Boolean) {
-                if (fromUser) {
-                    globalMediaPlayer.seekTo(progress)
-                }
-            }
-
-            override fun onStartTrackingTouch(seekBar: SeekBar?) {
-            }
-
-            override fun onStopTrackingTouch(seekBar: SeekBar?) {
-            }
-        })
-        val updateSeekBar = object : Runnable {
-            override fun run() {
-                val currentPosition = globalMediaPlayer.currentPosition
-                seekbar.progress = currentPosition
-                handler.postDelayed(this, 1000)
-            }
-        }
-        handler.postDelayed(updateSeekBar, 0)
     }
 }
