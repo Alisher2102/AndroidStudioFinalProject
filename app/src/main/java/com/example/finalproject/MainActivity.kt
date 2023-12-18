88ecc68c42def92e595dfee9241193e6a369f2b0
welray17 <temirlan2003@list.ru>
2023-12-18 23:40:49 +0600

    122

--- app/src/main/java/com/example/finalproject/MainActivity.kt
+++ app/src/main/java/com/example/finalproject/MainActivity.kt
@@ -4,14 +4,20 @@ import android.content.Intent
 import android.media.MediaPlayer
 import android.os.Bundle
 import android.util.Log
+import android.view.View
 import android.widget.ImageView
 import android.widget.SearchView
-import android.widget.SearchView.OnQueryTextListener
+import android.widget.SeekBar
 import androidx.activity.ComponentActivity
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.example.finalproject.data.MyPlaylistDataProvider
+import com.example.finalproject.model.MusicData
 import com.example.finalproject.model.Track
+import com.example.finalproject.network.MusicApi
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
 
 class MainActivity : ComponentActivity() {
     lateinit var recyclerView: RecyclerView
@@ -20,19 +26,17 @@ class MainActivity : ComponentActivity() {
     companion object {
         val globalMediaPlayer = MediaPlayer()
     }
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.list_of_music)
-        searchView = findViewById(R.id.search)
         recyclerView = findViewById(R.id.recyclerView)
-
         val dataList = MyPlaylistDataProvider.getTracksData()
         adapter = MyPlaylistAdapter(this@MainActivity, dataList)
         recyclerView.adapter = adapter
         recyclerView.layoutManager = LinearLayoutManager(this@MainActivity)
+        searchView = findViewById(R.id.search)
         Log.d("TAG: DataList", "Check : $dataList")
-        searchView.setOnQueryTextListener(object : OnQueryTextListener{
+        searchView.setOnQueryTextListener(object : SearchView.OnQueryTextListener {
             override fun onQueryTextSubmit(query: String?): Boolean {
                 return true
             }
